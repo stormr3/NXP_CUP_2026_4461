0ab3138f29edead0dba35f0b914a7dfaa2604d6d
Navdeeposm19 <navdeepsingh191005@gmail.com>
2026-08-13 13:50:57 +0530

    recovery from stuck , including parking
    Removed extra period in copyright notice.

--- b3rb_ros_line_follower.py
+++ b3rb_ros_line_follower.py
@@ -1,5 +1,5 @@
 # Copyright 2024-2026 NXP
-# Copyright 2016 Open Source Robotics Foundation, Inc.
+# Copyright 2016 Open Source Robotics Foundation, Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
